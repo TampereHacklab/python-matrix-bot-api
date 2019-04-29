0e80fb29a496e7aeef34adea60944546c1e80916
Ville Ranki <ville.ranki@iki.fi>
2019-04-29 17:26:20 +0300

    Fix teamup issues

--- modular_bot/modules/teamup.py
+++ modular_bot/modules/teamup.py
@@ -12,7 +12,7 @@ import os
 # TU_APIKEY - api key to use
 # TU_CALENDARS - list of calendar id's and rooms to notify about them
 #
-# Example: TU_CALENDARS="123abcdefg=#example:matrix.org,987asdfg=#other:matrix.org"
+# Example: TU_CALENDARS="123abcdefg#example:matrix.org,987asdfg#other:matrix.org"
 #
 
 class MatrixModule:
@@ -24,10 +24,10 @@ class MatrixModule:
         calendars = os.getenv("TU_CALENDARS").split(',')
 
         for calendarstring in calendars:
-            calendardef = calendarstring.split('=')
+            calendardef = calendarstring.split('#')
             calendar = Calendar(calendardef[0], self.api_key)
             calendar.timestamp = int(time.time())
-            calendar.notifyroom = calendardef[1]
+            calendar.notifyroom = '#' + calendardef[1]
             self.calendars.append(calendar)
             
         self.bot = bot
